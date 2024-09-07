1e9537f5b8a719fc1d77ff7808012ac4e5a1f441
a10ghute <A10GHUTE@GMAIL.COM>
2024-09-07 10:54:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Thank you for contacting us!');
+        form.reset();
+    });
+});
+
